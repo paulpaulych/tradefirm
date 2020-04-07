cc3891699ca9ac4767fd313a7cf9a13b4683b0fc
paulpaulych <p.pyankov@g.nsu.ru>
2020-04-07 17:43:56 +0700

    TRAD-6 + ci script

---
+++ server/src/test/kotlin/paulpaulych/tradefirm/GitlabCiTest.kt
@@ -0,0 +1,20 @@
+package paulpaulych.tradefirm
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FunSpec
+
+class GitlabCiTest: FunSpec() {
+
+    init {
+
+        test("correct"){
+            1 shouldBe 1
+        }
+
+        test("incorrect"){
+            1 shouldBe 2
+        }
+
+    }
+
+}
