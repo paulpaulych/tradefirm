b3dfbf1f215a50a99ddc58dc38d972a0bdb83c67
paulpaulych <p.pyankov@g.nsu.ru>
2020-05-26 19:38:31 +0700

    +server url fix

--- frontend/src/environments/environment.local.ts
+++ frontend/src/environments/environment.local.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: "http://192.168.0.151:3000/graphql",
-  authUrl: "http://192.168.0.151:3000/login"
+  backendUrl: "http://localhost:3000/graphql",
+  authUrl: "http://localhost:3000/login"
 };
